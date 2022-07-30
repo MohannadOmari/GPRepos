c358110c137ba2e902a41e5a0ef531ea3deca3d4
Mohannad <mohannad.alumari@gmail.com>
2022-07-30 08:27:22 +0300

    added sign in and changed sign up

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require('mongoose');
 const dbLogin = require("./connection/dbLogin");
+const cookieParser = require('cookie-parser');
 
 const authRoutes = require("./routes/auth");
 const auctionRoutes = require("./routes/auction");
@@ -15,6 +16,7 @@ app.set("views", "views");
 
 app.use(express.urlencoded({ extended: true })); // middleware
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
 
 // app.use("/", (req, res, next) => {
 // 	res.render("basic/home", {title: "Bidha Auction"});
@@ -49,44 +51,3 @@ app.listen(3000, () => {
  * Contact Us
  * Admin Panel
  */
-
-/* // auction testing
-const Auction = require('./models/auction');
-
-app.get('/add-auction', (req, res, next) => {
-	const auction = new Auction({
-		startDate: new Date("July 23, 2022 11:30:00"),
-		finishDate: new Date("July 24, 2022 05:00:00"),
-		cars: [
-			{
-				brand: 'BMW',
-				model: 'i8',
-				year: '2018',
-				fuel: 'electric',
-				mileage: 102478,
-				gearType: 'automatic',
-				interiorColor: 'White',
-				exteriorColor: 'white'
-			},
-			{
-				brand: 'Mercedes',
-				model: 'E200',
-				year: '2012',
-				fuel: 'petrol',
-				mileage: 64242,
-				gearType: 'automatic',
-				interiorColor: 'Gray',
-				exteriorColor: 'Black'
-			}
-		]
-	});
-
-	auction.save()
-		.then((result) => {
-			res.send(result)
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-
-}); */
--- models/bidder.js
+++ models/bidder.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -6,23 +8,27 @@ const bidderSchema = new Schema ({
     
             firstName: {
                 type: String,
-                required: true
+                required: [true, 'Please enter a first name']
             },
             lastName: {
                 type: String,
-                required: true
+                required: [true, 'Please enter a last name']
             },
             email: {
                 type: String,
-                required: true
+                required: [true, 'Please enter an email'],
+                unique: true,
+                lowercase: true,
+                validate: [isEmail, 'Please enter a valid email']
             },
             phoneNumber: {
-                type: Number,
-                //required: true
+                type: String,
+                required: [true, 'Please enter a phone number']
             },
             password: {
                 type: String,
-                required: true
+                required: [true, 'Please enter a password'],
+                minlength: [8, 'Minimum password length is 8 characters']
             },
             wallet: {
                 type: Number,
@@ -37,6 +43,27 @@ const bidderSchema = new Schema ({
             }*/
         
 }, { timestamps: true });
+
+// encrypt password when new bidder is created
+bidderSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+});
+
+// static method to login user
+bidderSchema.statics.signin = async function (email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+       const auth = await bcrypt.compare(password, user.password);
+       if (auth) {
+        return user;
+       }
+       throw Error('Incorrect password');
+    }
+    throw Error('Incorrect email');
+}
 
 const Bidder = mongoose.model('Bidder', bidderSchema);
 
